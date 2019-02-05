e8aa3e09bc99caa89213bdae5a26fb188b7ff608
EC2 Default User <ec2-user@ip-172-31-18-12.us-east-2.compute.internal>
2019-02-05 05:29:46 +0000

    Correction and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "ユーザー登録ありがとうございます!"
       redirect_to @user
     else
@@ -21,7 +22,6 @@ class UsersController < ApplicationController
   private
   
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end

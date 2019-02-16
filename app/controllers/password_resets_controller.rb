2b0a39d14550599a473296cdb2e5fd3a798ff322
EC2 Default User <ec2-user@ip-172-31-18-12.us-east-2.compute.internal>
2019-02-16 06:48:16 +0000

    Add password reset

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,63 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only:[:edit, :update]
+  
+  def new
+  end
+  
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "パスワード再設定用のメールが送信されました。"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "登録のないメールアドレスです。"
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+  
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "新しいパスワードが設定されました。"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+   private
+   
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # 正しいユーザーかどうか確認する
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+    
+    # トークンが期限切れかどうか確認する
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "パスワード再設定の期限が切れました。"
+        redirect_to new_password_reset_url
+      end
+    end
+end

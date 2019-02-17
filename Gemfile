d17a42cf77aa8a148492335b5d1d2388b335792f
EC2 Default User <ec2-user@ip-172-31-18-12.us-east-2.compute.internal>
2019-02-17 16:30:20 +0000

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.4.1'
 gem 'rails', '~> 5.2.0'
 gem 'kaminari'
 gem 'faker'
+gem 'carrierwave'
 gem 'jquery-rails'
 gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
@@ -22,7 +23,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt'
 
 # Use ActiveStorage variant
-# gem 'mini_magick', '~> 4.8'
+gem 'mini_magick', '~> 4.8'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -55,6 +56,7 @@ end
 
 group :production do
   gem 'pg'
+  gem 'fog'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index,:edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
    
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.page(params[:page])
     redirect_to root_url and return unless @user.activated?
   end
   
@@ -52,15 +53,6 @@ class UsersController < ApplicationController
     end
     
     # beforeアクション
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインして下さい。"
-        redirect_to login_url
-      end
-    end
-    
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
